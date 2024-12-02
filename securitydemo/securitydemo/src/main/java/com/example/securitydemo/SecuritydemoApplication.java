433b1a1b59d0735fdaece987bad84c4392dbc3ee
belvip <pouadjeubelvi@gmail.com>
2024-12-02 13:53:28 +0100

    Using Database to Store User Credentials with Spring Security

--- securitydemo/securitydemo/src/main/java/com/example/securitydemo/SecuritydemoApplication.java
+++ securitydemo/securitydemo/src/main/java/com/example/securitydemo/SecuritydemoApplication.java
@@ -3,7 +3,7 @@ package com.example.securitydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication  
+@SpringBootApplication
 public class  SecuritydemoApplication {
 
 	public static void main(String[] args) {
